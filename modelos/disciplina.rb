cef5d366194ab302e614f4cfeff1598593620acd
Gabriel Dolzan <gabrieldolzan@icloud.com>
2025-04-30 09:51:45 -0300

    Fixed Aluno

--- modelos/disciplina.rb
+++ modelos/disciplina.rb
@@ -4,6 +4,6 @@ ActiveRecord::Base.establish_connection :adapter => "sqlite3",
                                         :database => "Tabelas.sqlite3"
 
 class Disciplina < ActiveRecord::Base;
-    belongs_to :Aluno
+    belongs_to :aluno
     has_and_belongs_to_many :departamentos, -> {distinct}
 end
